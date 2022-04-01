8ee13d8eec414929226031909b9c4b1c8e015a6e
SuperSecureHuman <supersecurehuman@gmail.com>
2022-04-01 08:21:52 +0530

    Complete Assembler

--- README.md
+++ README.md
@@ -1,73 +1,17 @@
 # Hack assembler in python
 
-Untill this is done, I'll be updating this with the latest code input and output.
+This is a simple assembler for the Hack computer as defined in the course Nand2Tetris
 
+This assembler is written in pure python, and makes extensive use classes and functions.
 
-## Current status
+## Usage
 
-Now it can convert instructions to binary if the program dosent have symbols.
+    ```bash
+    python3 assembler.py <input.asm> 
+    ```
 
-Additionally, now it goes through the program and checks for new labels. Also adds them into the table.
+## License
 
+WTFPL 
 
-Input add.asm
-
-Output
-
-```Java
-@2
-0000000000000010
--------------------------- 
-
-D=A
-1110100110000000
--------------------------- 
-
-@3
-0000000000000011
--------------------------- 
-
-D=D+A
-1110100000010000
--------------------------- 
-
-@0
-0000000000000000
--------------------------- 
-
-M=D
-1110010001100000
--------------------------- 
-
-(var)
-D=M
-1110101110000000
--------------------------- 
-
-R0	0
-R1	1
-R2	2
-R3	3
-R4	4
-R5	5
-R6	6
-R7	7
-R8	8
-R9	9
-R10	10
-R11	11
-R12	12
-R13	13
-R14	14
-R15	15
-SCREEN	16384
-KBD	24576
-SP	0
-LCL	1
-ARG	2
-THIS	3
-THAT	4
-var	6
-
-
-```
+https://en.wikipedia.org/wiki/WTFPL
--- assembler.py
+++ assembler.py
@@ -1,12 +1,30 @@
-# run assembler.py Mult.asm in cli
+#!/home/venom/miniconda3/envs/python38/bin/python
 
+"""
+Assembler for the Hack computer language as defined by the course Nand2tetris
+
+    Usage:
+        assembler.py <input_file> 
+    Alternatively, you can edit the line after the import statement to change the input file.
+    The output file will be the same name as the input file, but with .hack extension.
+"""
+
+
+#Use Sys module to read the file name from the command line
 import sys
 
+#Read the first word in the command line input.
+#The first word is the name of the input file.
+file = sys.argv[1]
+#file = "Mult.asm"
 
-#file = sys.argv[1]
-file = "Mult.asm"
 
 class parser:
+
+    """
+    Init function to initialize the class, with needed variables,
+    and first to execute functions
+    """
 
     def __init__(self, inst):
         # Strip the instruction to remove leading and trailing spaces
@@ -28,14 +46,14 @@ class parser:
     def CheckType(self):
         # If Else to check the starting character of the instruction
         # And setting the type of the instruction
-        if self.inst.startswith('//') or self.inst == '':
+        if self.inst.startswith("//") or self.inst == "":
             return
-        if self.inst.startswith('('):
-            self.type = 'L'
-        elif self.inst.startswith('@'):
-            self.type = 'A'
+        if self.inst.startswith("("):
+            self.type = "L"
+        elif self.inst.startswith("@"):
+            self.type = "A"
         else:
-            self.type = 'C'
+            self.type = "C"
 
     """
     Read A value, and return the value only
@@ -47,11 +65,11 @@ class parser:
 
     def A_Value(self):
         # If inst is not A type, return none
-        if self.type != 'A':
+        if self.type != "A":
             return None
         # If its A type, then return the pure value of the instruction
         # I've used split to split the instruction into two parts, and take only the number part
-        self.valueA = self.inst[1:].split(' ')[0]
+        self.valueA = self.inst[1:].split(" ")[0]
         return self.valueA
 
     """
@@ -65,20 +83,20 @@ class parser:
 
     def RemoveCommentSpace(self):
         self.inst = self.inst.strip()
-        CommentLoc = self.inst.find('//')
+        CommentLoc = self.inst.find("//")
         # If the comment is not found, return the instruction as it is
         # find() returns -1 if the string is not found
         if CommentLoc == -1:
             self.inst = self.inst.strip()
         # If the first location is a //, then return blank string
         elif CommentLoc == 0:
-            self.inst = ''
+            self.inst = ""
         # If the comment is found, then return the instruction without the comment
         else:
             self.inst = self.inst[:CommentLoc].strip()
 
         # This will remove all the space in a line
-        self.inst = self.inst.replace(' ', '')
+        self.inst = self.inst.replace(" ", "")
 
     """
     Function to return true for blank lines
@@ -86,7 +104,7 @@ class parser:
 
     def RemoveBlankLine(self):
         # If the instruction is blank, return blank string
-        if self.inst == '':
+        if self.inst == "":
             return True
         # If the instruction is not blank, then return the instruction as it is
         else:
@@ -98,12 +116,12 @@ class parser:
 
     Input = self.inst
 
-    Output = 
+    Output = dest part of the instruction
     """
 
     def dst(self):
-        equalIndex = self.inst.find('=')
-        if self.type != 'C' or equalIndex == -1:
+        equalIndex = self.inst.find("=")
+        if self.type != "C" or equalIndex == -1:
             return
         else:
             self.destValue = self.inst[:equalIndex].strip()
@@ -119,17 +137,16 @@ class parser:
 
     def cmp(self):
         # Search for = and ;
-        equalLocation = self.inst.find('=')
-        semiColonLocation = self.inst.find(';')
+        equalLocation = self.inst.find("=")
+        semiColonLocation = self.inst.find(";")
 
-        if self.type != 'C':
+        if self.type != "C":
             return None
         # Checking through all the possible cases of = and ;, and getting the comp part of the instruction
         if equalLocation != -1 and semiColonLocation != -1:
-            self.compValue = self.inst[equalLocation +
-                                       1: semiColonLocation].strip()
+            self.compValue = self.inst[equalLocation + 1 : semiColonLocation].strip()
         elif equalLocation != -1 and semiColonLocation == -1:
-            self.compValue = self.inst[equalLocation+1:].strip()
+            self.compValue = self.inst[equalLocation + 1 :].strip()
         elif equalLocation == -1 and semiColonLocation != -1:
             self.compValue = self.inst[0:semiColonLocation].strip()
         elif equalLocation == -1 and semiColonLocation == -1:
@@ -145,10 +162,10 @@ class parser:
     """
 
     def jump(self):
-        semiColonLocation = self.inst.find(';')
-        if self.type != 'C' or semiColonLocation == -1:
+        semiColonLocation = self.inst.find(";")
+        if self.type != "C" or semiColonLocation == -1:
             return None
-        self.jumpValue = self.inst[semiColonLocation + 1:].strip()
+        self.jumpValue = self.inst[semiColonLocation + 1 :].strip()
         return self.jumpValue
 
 
@@ -160,16 +177,20 @@ class code:
     """
     Function to convert a given number into binary
 
-    Input = Anything
+    Input = Anything (int)
     
     Ouput = Binary
     """
 
     def decToBin(self, dec):
-        return format(dec, '016b')
+        return format(dec, "016b")
 
     """
     Convert A value to binary, just uses the earlier defined function
+
+    Input = A inst (without @)
+
+    Output = A inst in binary
     """
 
     def A_Value_Binary(self):
@@ -188,21 +209,21 @@ class code:
 
     def dst(self):
         if self.term == None:
-            self.destBin = '000'
-        elif self.term == 'M':
-            self.destBin = '001'
-        elif self.term == 'D':
-            self.destBin = '010'
-        elif self.term == 'MD':
-            self.destBin = '011'
-        elif self.term == 'A':
-            self.destBin = '100'
-        elif self.term == 'AM':
-            self.destBin = '101'
-        elif self.term == 'AD':
-            self.destBin = '110'
-        elif self.term == 'AMD':
-            self.destBin = '111'
+            self.destBin = "000"
+        elif self.term == "M":
+            self.destBin = "001"
+        elif self.term == "D":
+            self.destBin = "010"
+        elif self.term == "MD":
+            self.destBin = "011"
+        elif self.term == "A":
+            self.destBin = "100"
+        elif self.term == "AM":
+            self.destBin = "101"
+        elif self.term == "AD":
+            self.destBin = "110"
+        elif self.term == "AMD":
+            self.destBin = "111"
         return self.destBin
 
     """
@@ -215,94 +236,94 @@ class code:
     """
 
     def cmp(self):
-        a = '0'
-        c = ''
+        a = "0"
+        c = ""
         if self.term == None:
             self.compB = None
-        elif self.term == '0':
-            a = '0'
-            c = '101010'
-        elif self.term == '1':
-            a = '0'
-            c = '111111'
-        elif self.term == '-1':
-            a = '0'
-            c = '111010'
-        elif self.term == 'D':
-            a = '0'
-            c = '001100'
-        elif self.term == 'A':
-            a = '0'
-            c = '110000'
-        elif self.term == '!D':
-            a = '0'
-            c = '001101'
-        elif self.term == '!A':
-            a = '0'
-            c = '110001'
-        elif self.term == '-D':
-            a = '0'
-            c = '001111'
-        elif self.term == '-A':
-            a = '0'
-            c = '110011'
-        elif self.term == 'D+1':
-            a = '0'
-            c = '011111'
-        elif self.term == 'A+1':
-            a = '0'
-            c = '110111'
-        elif self.term == 'D-1':
-            a = '0'
-            c = '001110'
-        elif self.term == 'A-1':
-            a = '0'
-            c = '110010'
-        elif self.term == 'D+A':
-            a = '0'
-            c = '000010'
-        elif self.term == 'D-A':
-            a = '0'
-            c = '010011'
-        elif self.term == 'A-D':
-            a = '0'
-            c = '000111'
-        elif self.term == 'D&A':
-            a = '0'
-            c = '000000'
-        elif self.term == 'D|A':
-            a = '0'
-            c = '010101'
-        elif self.term == 'M':
-            a = '1'
-            c = '110000'
-        elif self.term == '!M':
-            a = '1'
-            c = '110001'
-        elif self.term == '-M':
-            a = '1'
-            c = '110011'
-        elif self.term == 'M+1':
-            a = '1'
-            c = '110111'
-        elif self.term == 'M-1':
-            a = '1'
-            c = '110010'
-        elif self.term == 'D+M':
-            a = '1'
-            c = '000010'
-        elif self.term == 'D-M':
-            a = '1'
-            c = '010011'
-        elif self.term == 'M-D':
-            a = '1'
-            c = '000111'
-        elif self.term == 'D&M':
-            a = '1'
-            c = '000000'
-        elif self.term == 'D|M':
-            a = '1'
-            c = '010101'
+        elif self.term == "0":
+            a = "0"
+            c = "101010"
+        elif self.term == "1":
+            a = "0"
+            c = "111111"
+        elif self.term == "-1":
+            a = "0"
+            c = "111010"
+        elif self.term == "D":
+            a = "0"
+            c = "001100"
+        elif self.term == "A":
+            a = "0"
+            c = "110000"
+        elif self.term == "!D":
+            a = "0"
+            c = "001101"
+        elif self.term == "!A":
+            a = "0"
+            c = "110001"
+        elif self.term == "-D":
+            a = "0"
+            c = "001111"
+        elif self.term == "-A":
+            a = "0"
+            c = "110011"
+        elif self.term == "D+1":
+            a = "0"
+            c = "011111"
+        elif self.term == "A+1":
+            a = "0"
+            c = "110111"
+        elif self.term == "D-1":
+            a = "0"
+            c = "001110"
+        elif self.term == "A-1":
+            a = "0"
+            c = "110010"
+        elif self.term == "D+A":
+            a = "0"
+            c = "000010"
+        elif self.term == "D-A":
+            a = "0"
+            c = "010011"
+        elif self.term == "A-D":
+            a = "0"
+            c = "000111"
+        elif self.term == "D&A":
+            a = "0"
+            c = "000000"
+        elif self.term == "D|A":
+            a = "0"
+            c = "010101"
+        elif self.term == "M":
+            a = "1"
+            c = "110000"
+        elif self.term == "!M":
+            a = "1"
+            c = "110001"
+        elif self.term == "-M":
+            a = "1"
+            c = "110011"
+        elif self.term == "M+1":
+            a = "1"
+            c = "110111"
+        elif self.term == "M-1":
+            a = "1"
+            c = "110010"
+        elif self.term == "D+M":
+            a = "1"
+            c = "000010"
+        elif self.term == "D-M":
+            a = "1"
+            c = "010011"
+        elif self.term == "M-D":
+            a = "1"
+            c = "000111"
+        elif self.term == "D&M":
+            a = "1"
+            c = "000000"
+        elif self.term == "D|M":
+            a = "1"
+            c = "010101"
         self.cmpBin = a + c
         return self.cmpBin
 
@@ -317,76 +338,113 @@ class code:
 
     def jump(self):
         if self.term == None:
-            self.jmpBin = '000'
-        elif self.term == 'JGT':
-            self.jmpBin = '001'
-        elif self.term == 'JEQ':
-            self.jmpBin = '010'
-        elif self.term == 'JGE':
-            self.jmpBin = '011'
-        elif self.term == 'JLT':
-            self.jmpBin = '100'
-        elif self.term == 'JNE':
-            self.jmpBin = '101'
-        elif self.term == 'JLE':
-            self.jmpBin = '110'
-        elif self.term == 'JMP':
-            self.jmpBin = '111'
+            self.jmpBin = "000"
+        elif self.term == "JGT":
+            self.jmpBin = "001"
+        elif self.term == "JEQ":
+            self.jmpBin = "010"
+        elif self.term == "JGE":
+            self.jmpBin = "011"
+        elif self.term == "JLT":
+            self.jmpBin = "100"
+        elif self.term == "JNE":
+            self.jmpBin = "101"
+        elif self.term == "JLE":
+            self.jmpBin = "110"
+        elif self.term == "JMP":
+            self.jmpBin = "111"
         return self.jmpBin
-    
+
 
 class passes:
-
     def __init__(self, symbol):
         self.symbol = symbol
+
+        """
+        First pass:
+        Reads the file line by line, reads for labels, and populates the symbol table
+
+        Input = File
+
+        Output = Labels in symbol table
+        """
+
     def firstPass(self):
-        with open(file, 'r') as asm:
+        # File handling
+        with open(file, "r") as asm:
             lineNo = -1
             for inst in asm:
+                # Parse the instruction (removes comments and does all the stuff)
                 p = parser(inst)
-                if p.type == 'A' or p.type == 'C':
+                # If type is A or C, skip
+                if p.type == "A" or p.type == "C":
                     lineNo += 1
-                if p.type == 'L':
+                    # If type is L (label) then it appends to symbol table
+                if p.type == "L":
                     sym = p.inst[1:-1]
+                    # Calls symbol checker, if not then adds it
                     if not self.symbol.CheckSymbolExist(sym):
-                       self.symbol.AddSymbols(sym, lineNo+1)
-    
-    def secondPass(self):
+                        self.symbol.AddSymbols(sym, lineNo + 1)
+        """
+        Second pass:
+        Here, it opens the file, creates the output file
+        Reads the input instruction, parses it Converts into machine code
+        and outputs to filename.hack
 
-        with open (file.split('.')[0] + '.hack', 'w') as hack:
-            with open (file, 'r') as asm:
+        Input = Asm file
+
+        Output = Hack file (fully done hack file)
+        """
+
+    def secondPass(self):
+        # Create the output file
+        with open(file.split(".")[0] + ".hack", "w") as hack:
+            # Open the input file
+            with open(file, "r") as asm:
+                # Variables in hack start from 16th mem location
                 variableLocation = 16
+                # For each line in input file
                 for inst in asm:
+                    # Parse the instruction (removes comments and does all the stuff)
                     input = parser(inst)
-                    if input.type == 'A':
+                    # Add/Check the symbol table for A inst
+                    if input.type == "A":
                         SYMBOL = input.inst[1:]
                         if self.symbol.CheckSymbolExist(SYMBOL):
                             out = code(self.symbol.GetSymbolValue(SYMBOL))
-                            hack.write(out.A_Value_Binary() + '\n')
+                            hack.write(out.A_Value_Binary() + "\n")
+                        # If symbol dosent exist, add it and output it
+                        # If the A inst is not a number, then it will throw an error
+                        # We will use that error to know that the A inst is not a number
+                        # Use error handling to put that inst into symbol table
                         else:
                             try:
                                 val = int(SYMBOL)
                                 out = code(val)
-                                hack.write(out.A_Value_Binary() + '\n')
+                                hack.write(out.A_Value_Binary() + "\n")
                             except ValueError:
                                 self.symbol.AddSymbols(SYMBOL, variableLocation)
                                 out = code(variableLocation)
-                                hack.write(out.A_Value_Binary() + '\n')
+                                hack.write(out.A_Value_Binary() + "\n")
                                 variableLocation += 1
-                    elif input.type == 'C':
+                    # C type inst is straight forward to add to the hack file
+                    elif input.type == "C":
                         d = code(input.dst())
                         c = code(input.cmp())
                         j = code(input.jump())
-                        hack.write('111'+c.cmp()+d.dst()+j.jump()+'\n') 
-
-
-
+                        hack.write("111" + c.cmp() + d.dst() + j.jump() + "\n")
 
 class symbol:
+
+    """
+    Init the symbol table
+    """
+
     def __init__(self):
         self.symbols = {}
+        # Loop to populate R0 to R15
         for MemReg in range(0, 16):
-            self.symbols['R' + str(MemReg)] = MemReg
+            self.symbols["R" + str(MemReg)] = MemReg
         self.symbols["SCREEN"] = 16384
         self.symbols["KBD"] = 24576
         self.symbols["SP"] = 0
@@ -395,20 +453,48 @@ class symbol:
         self.symbols["THIS"] = 3
         self.symbols["THAT"] = 4
 
+        """
+        Check if the symbol exists
+
+        Input = Symbol Name
+
+        Ouptut = Boolean
+        """
+
     def CheckSymbolExist(self, symbol):
         if self.symbols.get(symbol) == None:
             return False
         else:
             return True
+
+    """
+    Add symbols to the table
+
+    Input = Symbol Name, Location
+
+    Output = Add the symbol to the table
+    """
+
     def AddSymbols(self, symbol, value):
         self.symbols[symbol] = value
+
+    """
+    Get the value of the symbol
+
+    Input = Symbol Name
+
+    Output = Value of the symbol
+    """
+
     def GetSymbolValue(self, symbol):
         return self.symbols.get(symbol)
 
-def main():
-    st = symbol()
-    p = passes(st)
-    p.firstPass()
-    p.secondPass()
-
-main()
+# Finally, call all the functions to run the program
+# First we need to load the symbol table
+SymbolTable = symbol()
+# Feed the symbol table into the passes class
+Pass = passes(SymbolTable)
+# First Pass
+Pass.firstPass()
+# Second Pass
+Pass.secondPass()
